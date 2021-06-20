2228147ecbf5ea4933a7aee6c8b993be3f4f516a
Voland24 <luka.mladenovic@elfak.rs>
2021-06-20 21:50:20 +0200

    Dodao sam kontrolere koji treba

---
+++ APIagencijaNekretnine/Controllers/PoslovnicaController.cs
@@ -0,0 +1,323 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AgencijaNekretnine;
+
+namespace APIagencijaNekretnine.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class PoslovnicaController : ControllerBase
+    {
+       
+        [HttpGet]
+        [Route("PreuzmiSvePoslovnice/")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetSvePoslovnice()
+        {
+            try
+            {
+                return new JsonResult(DTOmanager.vratiSvePoslovnice());
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpGet]
+        [Route("PreuzmiPoslovnicu/{idPoslovnice}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetPoslovnicu(int idPoslovnice)
+        {
+            try
+            {
+                return new JsonResult(DTOmanager.vratiPoslovnicu(idPoslovnice));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpGet]
+        [Route("PreuzmiSveZaposlenePoslovnice/{idPoslovnice}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetSveZaposlene(int idPoslovnice)
+        {
+            try
+            {
+                return new JsonResult(DTOmanager.vratiZaposlenePoslovnice(idPoslovnice));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpGet]
+        [Route("PreuzmiProdavca/{jmbgProdavca}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetProdavca(string jmbgProdavca)
+        {
+            try
+            {
+                return new JsonResult(DTOmanager.vratiProdavca(jmbgProdavca));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpGet]
+        [Route("PreuzmiSveSefove/")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetSveSefove()
+        {
+            try
+            {
+                return new JsonResult(DTOmanager.vratiSveSefove());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpGet]
+        [Route("PreuzmiSefa/{jmbgSefa}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetSefa(string jmbgSefa)
+        {
+            try
+            {
+                return new JsonResult(DTOmanager.vratiSefa(jmbgSefa));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpGet]
+        [Route("PreuzmiAgenteProdavca/{jmbgProdavca}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetAgenteProdavca(string jmbgProdavca)
+        {
+            try
+            {
+                return new JsonResult(DTOmanager.vratiAgenteProdavca(jmbgProdavca));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+
+        [HttpGet]
+        [Route("PreuzmiAgenta/{jmbgAgenta}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetAgenta(string jmbgAgenta)
+        {
+            try
+            {
+                return new JsonResult(DTOmanager.vratiAgenta(jmbgAgenta));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpPost]
+        [Route("DodajProdavca/{idPoslovnice}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult DodajPoslovnicu(int idPoslovnice ,[FromBody]ZaposleniBasic z)
+        {
+            try
+            {
+                var p = DTOmanager.vratiPoslovnicu(idPoslovnice);
+                DTOmanager.dodajProdavca(z, p);
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpPost]
+        [Route("DodajSefa/{idPoslovnice}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult DodajSefa(int idPoslovnice, [FromBody] ZaposleniBasic z)
+        {
+            try
+            {
+                var p = DTOmanager.vratiPoslovnicu(idPoslovnice);
+                DTOmanager.dodajSefa(z, p);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpPost]
+        [Route("DodajPoslovnicu/")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult DodajPoslovnicu([FromBody]PoslovnicaBasic p)
+        {
+            try
+            {
+                DTOmanager.dodajPoslovnicu(p);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpPost]
+        [Route("DodajAgenta/")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult DodajAgenta([FromBody] AgentBasic a)
+        {
+            try
+            {
+                DTOmanager.dodajAgenta(a);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpPut]
+        [Route("IzmeniPoslovnicu")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult IzmeniPoslovnicu([FromBody] PoslovnicaBasic p)
+        {
+            try
+            {
+                DTOmanager.izmeniPoslovnicu(p);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpPut]
+        [Route("IzmeniProdavca")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult IzmeniProdavca([FromBody] ProdavacBasic p)
+        {
+            try
+            {
+                DTOmanager.izmeniProdavca(p);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpPut]
+        [Route("IzmeniAgenta")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult IzmeniAgenta([FromBody] AgentBasic a)
+        {
+            try
+            {
+                DTOmanager.izmeniAgenta(a);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpDelete]
+        [Route("IzbrisiPoslovnicu/{idPoslovnice}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult ObrisiPoslovnicu(int idPoslovnice)
+        {
+            try
+            {
+                DTOmanager.obrisiPoslovnicu(idPoslovnice);
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpDelete]
+        [Route("IzbrisiZaposlenog/{jmbgZaposlenog}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult ObrisiZaposlenog(string jmbgZaposlenog)
+        {
+            try
+            {
+                DTOmanager.obrisiProdavca(jmbgZaposlenog);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpDelete]
+        [Route("IzbrisiAgenta/{idAgenta}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult ObrisiAgenta(int idAgenta)
+        {
+            try
+            {
+                DTOmanager.obrisiAgenta(idAgenta);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ APIagencijaNekretnine/Controllers/UgovorController.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AgencijaNekretnine;
+using Newtonsoft.Json.Linq;
+
+namespace APIagencijaNekretnine.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UgovorController : ControllerBase
+    {
+        [HttpGet]
+        [Route("PreuzmiKupoprodajneUgovore")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetSveKPUgovore()
+        {
+            try
+            {
+                return new JsonResult(DTOmanager.vratiKPugovore());
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpGet]
+        [Route("PreuzmiIznajmljivanjeUgovore")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetSveIZNUgovore()
+        {
+            try
+            {
+                return new JsonResult(DTOmanager.vratiIZNugovore());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpPost]
+        [Route("DodajKupoprodajniUgovor")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public IActionResult DodajKPUgovor([FromBody]JObject data)
+        {
+            var jmbgKupca = data["jmbgKupca"].ToObject<string>();
+            var ulicaBroj = data["ulicaBroj"].ToObject<string>();
+            var jmbgProdavca = data["jmbgProdavca"].ToObject<string>();
+
+            try
+            {
+                DTOmanager.dodajKPugovor(jmbgKupca, ulicaBroj, jmbgProdavca);
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+
+
+        }
+
+        [HttpPost]
+        [Route("DodajIznajmljivanjeUgovor")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public IActionResult DodajIZNUgovor([FromBody] JObject data)
+        {
+            var jmbgKupca = data["jmbgKupca"].ToObject<string>();
+            var ulicaBroj = data["ulicaBroj"].ToObject<string>();
+            var jmbgProdavca = data["jmbgProdavca"].ToObject<string>();
+            var datum = data["datum"].ToObject<DateTime>();
+
+            try
+            {
+                DTOmanager.dodajIZNugovor(jmbgKupca, ulicaBroj, jmbgProdavca,datum);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+
+
+        }
+
+
+        [HttpDelete]
+        [Route("ObrisiKPUgovor/{ulica}/{broj}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public IActionResult ObrisiKPUgovor(string ulica, string broj)
+        {
+            try
+            {
+                DTOmanager.obrisiKP(ulica, broj);
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+        [HttpDelete]
+        [Route("ObrisiIZNUgovor/{ulica}/{broj}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public IActionResult ObrisiIZNUgovor(string ulica, string broj)
+        {
+            try
+            {
+                DTOmanager.obrisiIZN(ulica, broj);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+
+
+
+
+
+    }
+}
